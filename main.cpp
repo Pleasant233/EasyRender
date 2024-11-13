77f4b4a5971cefef9026c9205b9e70938fa283c4
Pleasant233 <165742912+Pleasant233@users.noreply.github.com>
2024-11-13 11:35:07 +0800

    线框部分结束

--- main.cpp
+++ main.cpp
@@ -6,20 +6,36 @@
 
 
 #include"line.h"
+Model* model;
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
-const int width = 1920;
+const int width = 1080;
 const int height = 1080;
-int main()
+int main(int argc,char** argv)
 {
+    if (2 == argc) {
+       model = new Model(argv[1]);
+    }
+    else{
+       model = new Model("obj/AppleForEasyRender.obj");
+    }
 	TGAImage image(width,height,TGAImage::RGB);
-	Vec2i p1(0, 0);
-	Vec2i p2(1920, 1080);
-	Vec2i p3(0, 1080);
-	Vec2i p4(1920, 0);
-	line(p1, p2,image, red);
-	line(p3, p4, image, green);
-	image.write_tga_file("Line.tga");
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2;
+            int y0 = (v0.y + 1.) * height / 2;
+            int x1 = (v1.x + 1.) * width / 2;
+            int y1 = (v1.y + 1.) * height / 2;
+            Vec2i v0v = { x0,y0 };
+            Vec2i v1v = { x1,y1 };
+            line(v0v,v1v,image, white);
+        }
+    }
+    image.flip_vertically();
+	image.write_tga_file("AppleWithLine.tga");
 	return 0;
 }
